5b5e36d2fb1182065b0eb30e86e48cb075aded76
João Mendes <joaopedrolopesmendes@gmail.com>
2023-08-01 16:43:43 +0000

    +ADD add auth flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
@@ -106,7 +107,11 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+          
         </a>
+
+        <UserButton afterSignOutUrl="/"/>
+
       </div>
     </main>
   )
